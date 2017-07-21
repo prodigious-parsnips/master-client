80bc11f098842e4277e5f10699a298366fdefeb0
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-07-21 16:45:57 -0700

    added basic text inputs to sign in form (#2)

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { SocialIcon } from 'react-native-elements'
 
 // https://github.com/oblador/react-native-vector-icons#installation
@@ -8,10 +8,12 @@ export default SignIn = () => (
   <View>
     <Text>Sign In!!!</Text>
     <View style={styles.row}>
+      <TextInput style={styles.login} placeholder="Login" />
+      <TextInput style={styles.pass} placeholder="Password" />
       <SocialIcon title='Sign In With Facebook' button type='facebook' onPress={() => console.log('hello')}/>
       <SocialIcon title='Sign In With Github' button type='github' />
       <SocialIcon title='Sign In With Twitter' button type='twitter' />
-      <SocialIcon style={styles.google} title='Sign In With Google' button type='google' />
+      <SocialIcon title='Sign In With Google' button type='google' style={styles.google} />
     </View>
 
   </View>
@@ -21,6 +23,19 @@ const styles = StyleSheet.create({
   row: {
     flex: 1,
     flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  login: {
+    height: 40,
+    borderWidth: 1.5,
+    borderRadius: 5,
+  },
+  pass: {
+    marginTop: 2,
+    height: 40,
+    borderWidth: 1.5,
+    borderRadius: 5,
   },
   google: {
     backgroundColor: 'red',
