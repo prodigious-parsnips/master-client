a1ab25d7e5de7fbe4bffe9c5232b3c78eb1e04f5
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-07-21 19:58:09 -0700

    get rid of testing errors (#4)

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,8 +1,8 @@
 import 'react-native';
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
+
 // components for testing
-import HomeScreen from '../HomeScreen.js'
 import PostList from '../src/PostList.js';
 import CreatePosts from '../src/CreatePosts.js';
 import PostPreview from '../src/PostPreview.js';
@@ -14,11 +14,35 @@ import UserSettings from '../src/settings/UserSettings.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { HomeScreen } from '../index.ios.js'
+import Drawer from '../src/Drawer.js';
 
 describe('HomeScreen', () => {
-  it('renders correctly', () => {
+  it('renders HomeScreen correctly', () => {
     const tree = renderer.create(
-      <HomeScreen />
+      HomeScreen: {
+        screen: HomeScreen,
+      }
+    );
+  });
+});
+
+describe('Drawer', () => {
+  it('renders Drawer correctly', () => {
+    const tree = renderer.create(
+      Drawer: {
+        screen: Drawer,
+      }
+    );
+  });
+});
+
+describe('PostList', () => {
+  it('renders PostList correctly', () => {
+    const tree = renderer.create(
+      PostList: {
+        screen: PostList,
+      }
     );
   });
 });
