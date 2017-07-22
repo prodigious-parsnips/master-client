c3e8c5a0ecfbe6a0833a7d093408cbd5c95b077f
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-07-22 15:50:45 -0700

    implement expanding settings for user settings and admin settings

--- src/settings/Settings.js
+++ src/settings/Settings.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import Accordion from 'react-native-collapsible/Accordion';
 
 import UserSettings from './UserSettings.js';
 import AdminSettings from './AdminSettings.js';
+
+
 
 export default class Settings extends React.Component {
   static navigationOptions = {
     title: 'Settings',
   };
+
+  renderHeader(section) {
+    return (
+      <View style={styles.header}>
+        <Text style={styles.title}>Title - Click Me</Text>
+      </View>
+    );
+  }
+
+  renderContent(section) {
+    return (
+      <View>
+        <UserSettings />
+      </View>
+    );
+  }
+
+  renderContent2(section) {
+    return (
+      <View>
+        <AdminSettings />
+      </View>
+    );
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -16,7 +44,33 @@ export default class Settings extends React.Component {
         <Text>Settings!!!</Text>
         <Button title="View User Settings" color="#000000" onPress={() => navigate('UserSettings')} />
         <Button title="View Admin Settings" color="#000000" onPress={() => navigate('AdminSettings')} />
+
+        <Text>Admin Settings Panels</Text>
+        <Accordion
+          sections={['Section 1', 'Section 2', 'Section 3']}
+          renderHeader={this.renderHeader}
+          renderContent={this.renderContent2}
+        />
+
+        <Text>User Settings Panels</Text>
+        <Accordion
+          sections={['Section 1', 'Section 2', 'Section 3']}
+          renderHeader={this.renderHeader}
+          renderContent={this.renderContent}
+        />
       </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    backgroundColor: 'rgba(0, 124, 220, 100)',
+    borderColor: 'red',
+    borderStyle: 'solid'
+  },
+});
