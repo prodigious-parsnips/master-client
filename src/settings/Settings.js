6cb4c4aba28e33349710e1c1005a7fe8521f4632
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-07-23 09:02:14 -0700

    update settings render functions

--- src/settings/Settings.js
+++ src/settings/Settings.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+
 import Accordion from 'react-native-collapsible/Accordion';
 
 import UserSettings from './UserSettings.js';
 import AdminSettings from './AdminSettings.js';
-
 
 
 export default class Settings extends React.Component {
@@ -21,18 +20,18 @@ export default class Settings extends React.Component {
     );
   }
 
-  renderContent(section) {
+  renderAdminSettings(section) {
     return (
       <View>
-        <UserSettings />
+      <AdminSettings />
       </View>
     );
   }
 
-  renderContent2(section) {
+  renderUserSettings(section) {
     return (
       <View>
-        <AdminSettings />
+        <UserSettings />
       </View>
     );
   }
@@ -41,22 +40,20 @@ export default class Settings extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <ScrollView>
-        <Text>Settings!!!</Text>
-        <Button title="View User Settings" color="#000000" onPress={() => navigate('UserSettings')} />
-        <Button title="View Admin Settings" color="#000000" onPress={() => navigate('AdminSettings')} />
+        <Text>Settings Screen</Text>
 
         <Text>Admin Settings Panels</Text>
         <Accordion
           sections={['Section 1', 'Section 2', 'Section 3']}
           renderHeader={this.renderHeader}
-          renderContent={this.renderContent2}
+          renderContent={this.renderAdminSettings}
         />
 
         <Text>User Settings Panels</Text>
         <Accordion
           sections={['Section 1', 'Section 2', 'Section 3']}
           renderHeader={this.renderHeader}
-          renderContent={this.renderContent}
+          renderContent={this.renderUserSettings}
         />
       </ScrollView>
     );
