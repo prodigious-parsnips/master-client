aa4b843ae79da9e3eaff59d09c7de4eaf431902a
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-07-24 15:57:35 -0700

    refactor admin settings

--- src/settings/AdminSettings.js
+++ src/settings/AdminSettings.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import { Text, View, Slider, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Slider, TextInput } from 'react-native';
 
-export default AdminSettings = () => (
-  <View>
-    <Text>AdminSettings Component</Text>
-    <View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
-      <Text>Title:</Text>
-      <TextInput
-        style={{height: 40, width:200, borderWidth: 1, borderRadius: 5}}
-        placeholder="Title"
-      />
-      <Text>Description:</Text>
-      <TextInput
-        style={{height: 100, width:200, borderWidth: 1, borderRadius: 5}}
-        placeholder="Description"
-      />
-      <Text>Upvote Threshold</Text>
-      <Slider style={{width:200}}/>
-      <Text>Distance Threshold</Text>
-      <Slider style={{width:200}}/>
-    </View>
-  </View>
-);
+export default class AdminSettings extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>AdminSettings Component</Text>
+        <View style={styles.component}>
+          <Text>Title:</Text>
+          <TextInput style={styles.title} placeholder="Title" />
+          <Text>Description:</Text>
+          <TextInput style={styles.description} placeholder="Description" />
+          <Text>Upvote Threshold</Text>
+          <Slider style={{width:200}}/>
+          <Text>Distance Threshold</Text>
+          <Slider style={{width:200}}/>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  component: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  title: {
+    height: 40,
+    width: 200,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  description: {
+    height: 100,
+    width: 200,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+})
