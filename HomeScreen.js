d7487969efed6d411e2467ff9cf4e271b0f40616
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-07-27 11:58:42 -0700

    Feat/settings fetch (#18)
    * implement fetch post admin upvote and location sliders
    
    * implement native base tabs to sign in
    
    * implement push notifications

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { Button } from 'react-native-elements';
 import Drawer from './src/DrawerNav.js'
+// var PushNotification = require('react-native-push-notification');
+import PushNotification from 'react-native-push-notification';
 
 import PostPreview from './src/PostPreview.js';
 import PostList from './src/PostList.js';
@@ -10,16 +13,49 @@ import PostList from './src/PostList.js';
 export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
+
+    this.testPush = this.testPush.bind(this);
   }
+
   static navigationOptions = {
     title: 'Home',
   };
+
+  componentDidMount() {
+    PushNotification.configure({
+      // (required) Called when a remote or local notification is opened or received
+      onNotification: function(notification) {
+          console.log( 'NOTIFICATION:', notification );
+      },
+      // IOS ONLY (optional): default: all - Permissions to register.
+      permissions: {
+          alert: true,
+          badge: true,
+          sound: true
+      },
+      // Should the initial notification be popped automatically
+      // default: true
+      popInitialNotification: true,
+
+      requestPermissions: true,
+    });
+  }
+
+
+  testPush() { 
+    PushNotification.localNotificationSchedule({
+      message: "HereNow Notification Message", // (required)
+      number: 1,
+      date: new Date(Date.now() + (3 * 1000)) // in 3 secs
+    });     
+  }
 
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.app}>
         <PostList navigation={this.props.navigation} fetchMessages={this.props.screenProps.fetchMessages} messages={this.props.screenProps.messages}/>
+        <Button raised title='Test Push' onPress={this.testPush} />
       </View>
     );
   }
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,42 +1,57 @@
 import React from 'react';
 import { StyleSheet, Text, ScrollView, View, TextInput } from 'react-native';
-import { Button } from 'react-native-elements'
+import { Button } from 'react-native-elements';
+import { Container, Header, Content, Tab, Tabs } from 'native-base';
 
 // https://github.com/oblador/react-native-vector-icons#installation
 
 export default SignIn = (props) => (
-  <ScrollView>
-    <View style={styles.row}>
-      <Text>Sign In</Text>
-      <TextInput style={styles.login} placeholder="Username"
-        onChangeText={(text)=>{props.screenProps.handleSignInActions('username', text)}}
-      />
-      <TextInput style={styles.pass} placeholder="Password"
-        onChangeText={(text)=>{props.screenProps.handleSignInActions('password', text)}}
-      />
 
-      <Button
-        raised iconRight icon={{name: 'person'}}
-        title='Sign Into HereNow' buttonStyle={styles.button}
-        onPress={()=>{props.screenProps.handleSignInActions('submit')}}
-      />
+  <Container>
+    
+  <Header hasTabs />
 
-      <Text>Sign Up</Text>
-      <TextInput style={styles.login} placeholder="Create Username"
-        onChangeText={(text)=>{props.screenProps.handleSignUpActions('username', text)}}
-      />
-      <TextInput style={styles.pass} placeholder="Create Password"
-        onChangeText={(text)=>{props.screenProps.handleSignUpActions('password', text)}}
-      />
+    <Tabs initialPage={0}>
 
-      <Button
-        raised iconRight icon={{name: 'person-add'}}
-        title='Sign Up For HereNow' buttonStyle={styles.button}
-        onPress={()=>{props.screenProps.handleSignUpActions('submit')}}
-      />
-    </View>
+      <Tab heading="Sign In">
+        <View style={styles.row}>
+          <Text>Sign In</Text>
+          <TextInput style={styles.login} placeholder="Username"
+            onChangeText={(text)=>{props.screenProps.handleSignInActions('username', text)}}
+          />
+          <TextInput style={styles.pass} placeholder="Password"
+            onChangeText={(text)=>{props.screenProps.handleSignInActions('password', text)}}
+          />
 
-  </ScrollView>
+          <Button
+            raised iconRight icon={{name: 'person'}}
+            title='Sign Into HereNow' buttonStyle={styles.button}
+            onPress={()=>{props.screenProps.handleSignInActions('submit')}}
+          />
+        </View>
+      </Tab>
+
+      <Tab heading="Sign Up">
+        <View style={styles.row}>
+          <Text>Sign Up</Text>
+          <TextInput style={styles.login} placeholder="Create Username"
+            onChangeText={(text)=>{props.screenProps.handleSignUpActions('username', text)}}
+          />
+          <TextInput style={styles.pass} placeholder="Create Password"
+            onChangeText={(text)=>{props.screenProps.handleSignUpActions('password', text)}}
+          />
+
+          <Button
+            raised iconRight icon={{name: 'person-add'}}
+            title='Sign Up For HereNow' buttonStyle={styles.button}
+            onPress={()=>{props.screenProps.handleSignUpActions('submit')}}
+          />
+        </View>
+      </Tab>
+
+    </Tabs> 
+
+  </Container>
 );
 
 const styles = StyleSheet.create({
