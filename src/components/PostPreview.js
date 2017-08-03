b745450e4a4600e6dc7f1015b5ae5eadf2983385
Rob <robstlezin@gmail.com>
2017-08-03 14:09:50 -0700

    added styles to settings and create map

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -6,7 +6,12 @@ export default PostPreview = (props) => (
   <View style={styles.row}>
     <Image source={{uri: 'https://s-media-cache-ak0.pinimg.com/originals/3f/28/54/3f2854b8c2962f0938050ff775129d47.jpg'}}
     style={styles.image} />
-  <Text>{props.title}</Text>
+  <Text style={styles.title}>{props.title}</Text>
   </View>
 );
 //
+
+// const RobsStyle = StyleSheet.create({
+//     obj: {
+//     }
+// })
